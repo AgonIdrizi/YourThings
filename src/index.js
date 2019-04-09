ee81cb9fd4e28e9f2af94cfd4ae3ae18927b6266
AgonIdrizi <agon.88@hotmail.com>
2019-04-09 20:48:31 +0200

    style buttons

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { renderProjects } from './renderProjects'
 import { removeProject } from './removeProject'
 import { addProjectInDom } from './addProjectInDom'
 import { addTodoInDom } from './addTodoInDom'
+import { addProjectInLocalStorage } from './addProjectInLocalStorage'
 import  MicroModal  from 'micromodal'
 
 
@@ -17,26 +18,24 @@ import '@fortawesome/fontawesome-free/js/brands'
 
 console.log('ss');
 
-var todo1 = new todo(1, 'home','we need to cleanfff fffffffffffffffffffffffffff', format(new Date(2019,17,4),'YYYY/MMM/D'),'normal')
-var todo2 = new todo(2, 'run','on the weekend',format(new Date(2019,17,4),'YYYY/MMM/D'),'high')
-var project1 = new project('test')
+//var todo1 = new todo(1, 'home','we need to cleanfff fffffffffffffffffffffffffff', format(new Date(2019,17,4),'YYYY/MMM/D'),'normal')
+//var todo2 = new todo(2, 'run','on the weekend',format(new Date(2019,17,4),'YYYY/MMM/D'),'high')
+if (localStorage.getItem('projects') == null){
+  var project1 = new project('test')
+  addProjectInLocalStorage(project1)
+}
+//localStorage.setItem('projects',"1,test")
+//default project
 
 
-console.log(project1._projectId)
-console.log(todo1._description)
 
-localStorage.setItem('1',todo1.values)
-localStorage.setItem('2',todo2.values)
 
-console.log(getToDosOfaProject(1))
+//console.log(getToDosOfaProject(1))
 renderProjects()
 
 var addProjectButton = document.querySelector('.add-project').lastElementChild
 console.log(addProjectButton)
 addProjectButton.addEventListener('click',addProjectInDom)
-
-
-
 
 
 var newTodoButton = document.querySelector('.add-todo').firstElementChild
